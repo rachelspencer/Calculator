1fe898dae74e64d3ffd49cf64b2a6872f86ebe5e
Rachel Spencer <rachelspencer688@gmail.com>
2022-08-20 17:23:31 -0400

    Working Calcultor

---
+++ calc.js
@@ -0,0 +1,44 @@
+// const { default: nodeTest } = require('node:test');
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	const tokens = input.split(' ');
+	console.log('input', input);
+	const mathSymbol = tokens[0];
+	const num1 = Number(tokens[1]);
+	const num2 = Number(tokens[2]);
+
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
+
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	}
+
+	if (mathSymbol === "-"){
+		console.log(num1 - num2);
+	}
+
+	if (mathSymbol === "*"){
+		console.log(num1 * num2);
+	}
+
+	if (mathSymbol === "/"){
+		console.log(num1 / num2);
+	}
+
+	if (mathSymbol === "√" ){
+		console.log(Math.sqrt(num1));
+	}
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
