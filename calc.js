3211cace82e726a5893e3eb0acd419dbf675a554
Rachel Spencer <rachelspencer688@gmail.com>
2022-08-20 21:08:26 -0400

    Initial commit

--- calc.js
+++ calc.js
@@ -15,31 +15,10 @@ reader.question("What would you like to calculate?", function(input){
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
 
-	console.log('mathSymbol', mathSymbol);
-	console.log('num1', num1);
-	console.log('num2', num2);
 
-	if (mathSymbol === "+"){
-		console.log(num1 + num2);
-	}
-
-	if (mathSymbol === "-"){
-		console.log(num1 - num2);
-	}
-
-	if (mathSymbol === "*"){
-		console.log(num1 * num2);
-	}
-
-	if (mathSymbol === "/"){
-		console.log(num1 / num2);
-	}
-
-	if (mathSymbol === "√" ){
-		console.log(Math.sqrt(num1));
 	}
 	// This line closes the connection to the command line interface.
 	reader.close()
-// testing git proc
+
 
 });
