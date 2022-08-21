142698c1ac3ca4c3ab4fa6d37528b070240dfc86
Rachel Spencer <rachelspencer688@gmail.com>
2022-08-20 21:56:09 -0400

    Sqrt implementation working

--- calc.js
+++ calc.js
@@ -34,6 +34,10 @@ reader.question("What would you like to calculate?", function(input){
 	if (mathSymbol === "/"){
 		console.log(num1 / num2);
 	}
+
+	if (mathSymbol === "√" ){
+		console.log(Math.sqrt(num1));
+	}
 	// This line closes the connection to the command line interface.
 	reader.close()
 
